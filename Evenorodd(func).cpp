083525bcb51e4a294ab354bac91ccf3edaa58b7e
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-06-01 09:17:02 +0530

    Add files via upload

---
+++ Evenorodd(func).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool iseven(int x)
+{
+	if(x%2==0)
+	{
+		cout<<"even";
+	}
+	else
+	{
+		cout<<"odd";
+	}
+}
+int main()
+{
+	int n;
+	cin>>n;
+	iseven(n);
+}
